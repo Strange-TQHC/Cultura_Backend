f027b1a1bcea31d2c75c9cdbddda598947056dc2
Kaushik Kalyan Bojja <thequadheadedcoder@gmail.com>
2026-04-07 11:41:58 +0530

    Added User model, serializer, and signup API

--- cultura_backend/users/models.py
+++ cultura_backend/users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=20)
+
+    current_location = models.CharField(max_length=255)
+    permanent_location = models.CharField(max_length=255)
+
+    food_preferences = models.TextField()
+
+    def __str__(self):
+        return self.email
--- cultura_backend/users/views.py
+++ cultura_backend/users/views.py
@@ -1,6 +1,15 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .models import User
+from .serializers import UserSerializer
 
-@api_view(['GET'])
-def test_api(request):
-    return Response({"message": "CULTURA API is working"})
+@api_view(['POST'])
+def signup(request):
+    serializer = UserSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response({"message": "User created successfully"})
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
